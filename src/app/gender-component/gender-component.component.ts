aeb05b0d0034dfd5067c58048793cd89f976b39f
DESKTOP-FGVIK5K\Sok Meng <mmeng0976@gmail.com>
2025-04-28 14:16:58 +0700

    Create Component Gender in branch dev_pro

---
+++ src/app/gender-component/gender-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gender-component',
+  imports: [],
+  templateUrl: './gender-component.component.html',
+  styleUrl: './gender-component.component.css'
+})
+export class GenderComponentComponent {
+
+}
