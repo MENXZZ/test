becd5ca893b9782a096b692d6707119d219cdd76
DESKTOP-FGVIK5K\Sok Meng <mmeng0976@gmail.com>
2025-04-29 10:21:23 +0700

    fix code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GenderComponentComponent } from './gender-component/gender-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GenderComponentComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
